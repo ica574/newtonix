4468dbc7f84e371e2e63ee54cd375659769eae75
Isaac Cilia Attard <iciliaat@gmail.com>
2023-01-27 10:38:28 +0100

    Newline character support
    Added support for '\n' in 'terminal_putchar()' of 'tty.c'. Also changed terminal emulator colour to green.

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -19,7 +19,7 @@ static uint16_t* terminal_buffer;
 void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
 	terminal_buffer = VGA_MEMORY;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -39,6 +39,10 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 }
 
 void terminal_putchar(char c) {
+	if (c == '\n') {
+		terminal_row++;
+		terminal_column = 0;
+	}
 	unsigned char uc = c;
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
