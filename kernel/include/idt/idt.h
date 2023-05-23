a6e1a57732581e917b3c9b17d9c52f0770b2bde4
Isaac Cilia Attard <iciliaat@gmail.com>
2023-05-23 16:35:58 +0200

    Freestanding binary
    Created freestanding binary in Rust.

--- kernel/include/idt/idt.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void idt_init();
-void idt_disablegate(int interrupt);
-void idt_enablegate(int interrupt);
-void idt_setgate(int interrupt, void *base, uint16_t segmentDescriptor, uint8_t flags);
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![no_std] // Removes standard library, still includes C runtime
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
